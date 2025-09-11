80a92aa53d86dbdc7b2d5bdcc091dcac099988d6
Madalina Bulat <jmfhpca@jmfamily.com>
2025-09-11 10:35:04 -0400

    test commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Game
+{
+    //questions
+    //category
+    //correctanswer
+    //options
+    //difficulty
+
+    //makegame()
+    //api call
+
+    //isover()
+
+
+}
+
+class Program
+{
+    static async Task Main(strings[] args)
+    {
+        //// have a cool console WELCOME TO TRIVIA sign or something
+        //Console.ForegroundColor = ConsoleColor.Cyan;
+        //Console.WriteLine("┌───────────────────────────────┐");
+        //Console.WriteLine("│      Welcome to Trivia!       │");
+        //Console.WriteLine("└───────────────────────────────┘\n");
+        //Console.ResetColor();
+
+
+        // loop for game
+        // while(!game.IsOver())
+    }
+}
