884cf663fb8b214a34bd92eaed86673b44c549af
Madalina Bulat <jmfhpca@jmfamily.com>
2025-09-11 11:07:51 -0400

    modified trivia.cs file

--- Trivia.cs
+++ Trivia.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class TriviaGame
+{
+    public List<Question> Questions { get; set; } = new();
+    public int turn { get; set; } = 0;
+
+    public void MakeGame()
+    {
+        using var client = new HttpClient();
+        string url = $"https://opentdb.com/api.php?amount=10";
+       
+        var response = await client.GetStringAsync(url);
+        responseTask.Wait();
+        string response = responseTask.Result;
+
+     
+
+       // public bool IsOver() => turn 
+    }
