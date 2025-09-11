faedc030a2f7058475301b53240ff16ab5926ef1
Isaac Adrian <jmfikwi@jmfamily.com>
2025-09-11 11:27:06 -0400

    built constructor

--- Trivia.cs
+++ Trivia.cs
@@ -1,9 +1,42 @@
 ﻿public class TriviaGame
 {
-    public List<Question> Questions { get; set; } = new();
-    public int turn { get; set; } = 0;
+    public List<Question> Questions { get; set; }
 
-    public void MakeGame()
+    public List<Player> Players { get; set; }
+    public int Turn { get; set; }
+
+    public int NumQuestions { get; set; }
+
+    public int NumPlayers { get; set; }
+
+    public Difficulties Difficulty { get; set; }
+
+    public Categories Category { get; set; }
+
+    public TriviaGame(int numQuestions=10, int numPlayers=1, string difficultyInput="Easy", string categoryInput="All")
+    {
+        NumQuestions = numQuestions;
+        NumPlayers = numPlayers;
+
+        if (!Enum.TryParse(difficultyInput, true, out Difficulties difficulty))
+        {
+            Console.WriteLine("Invalid difficulty. Defaulting to Easy.");
+            difficulty = Difficulties.Easy;
+        }
+        Difficulty = difficulty;
+
+        if (!Enum.TryParse(difficultyInput, true, out Categories category))
+        {
+            Console.WriteLine("Invalid category. Defaulting to Easy.");
+            category = Categories.All;
+        }
+        Category = category;
+    }
+
+
+
+
+public void MakeGame()
     {
         using var client = new HttpClient();
         string url = $"https://opentdb.com/api.php?amount=10";
