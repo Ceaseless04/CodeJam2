58474aefad29a3fa56451d303cdd805ef287718b
Samantha Rivera <jmfaryl@jmfamily.com>
2025-09-11 10:49:32 -0400

    remove main

--- obj/Release/net9.0/CodeJam2.AssemblyInfo.cs
+++ obj/Release/net9.0/CodeJam2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeJam2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614465ef8f89ce15663c5a6db97a6f1626093318")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeJam2")]
