614465ef8f89ce15663c5a6db97a6f1626093318
Isaac Adrian <jmfikwi@jmfamily.com>
2025-09-11 10:47:30 -0400

    Console library

--- obj/Debug/net9.0/CodeJam2.AssemblyInfo.cs
+++ obj/Debug/net9.0/CodeJam2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeJam2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a92aa53d86dbdc7b2d5bdcc091dcac099988d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4099a84a676c5723f468cdce59fd94c792d18623")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeJam2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeJam2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
