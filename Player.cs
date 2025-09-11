23128f6c25b0b961bf1dad32cdd8117fb72106dc
Samantha Rivera <jmfaryl@jmfamily.com>
2025-09-11 11:15:10 -0400

    player class

--- Player.cs
+++ Player.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Player
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+
+    private static int _numberOfPlayers = 0;
+
+    public Player(string name)
+    {
+        Name = name;
+        Score = 0;
+        _numberOfPlayers++;
+    }
+}
